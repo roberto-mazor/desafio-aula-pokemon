7e3238d28059498322ae8544fbd880f2664ebb3a
roberto-mazor <mazor.estudo@gmail.com>
2025-07-31 20:36:57 -0300

    Feat: Criado looping com map
    iniciando looping usando map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
+import Card from './Card';
 
-function App() {
-  
+  function App() {
+  const item1 = {
+    nome:'Bumbasauro', 
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+  }
 
+  const item2 = {
+    nome:'Bumbasauro', 
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
+  }
+
+  const item3 = {
+    nome:'Bumbasauro', 
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
+  }
+
+  const personagens = [item1, item2, item3]
   return (
     <>
-      
+       <div className='lista'>
+          {
+          personagens.map(function(umPersonagem){
+            return <Card info={umPersonagem}/>
+          })
+         }
+         </div>
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Card.css'
+
+function Card(props) {
+    return(
+        <>
+            <div className='card'> 
+                <h2>{props.info.nome}</h2>
+                <img src={props.info.foto}/>
+            </div>
+        </>
+    )
+}
+
+export default Card
