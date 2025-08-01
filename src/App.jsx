cf1c58d04c0098cf98fcca4ea03511abf9cf0b41
roberto-mazor <mazor.estudo@gmail.com>
2025-07-31 21:42:22 -0300

    feat: Adicionado lista de cards
    Adicionado mais itens na lista

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,69 @@ import Card from './Card';
 
   function App() {
   const item1 = {
-    nome:'Bumbasauro', 
+    id:1,
+    nome:'Bulbasaur', 
+    tipo: 'Planta',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
   }
 
   const item2 = {
-    nome:'Bumbasauro', 
+    id:2,
+    nome:'Ivysaur',
+    tipo: 'Planta',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
   }
 
   const item3 = {
-    nome:'Bumbasauro', 
+    id:3,
+    nome:'Venusaur', 
+    tipo: 'Planta',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
   }
 
-  const personagens = [item1, item2, item3]
+const item4 = {
+    id:4,
+    nome:'Charmander', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+  }
+
+  const item5 = {
+    id:5,
+    nome:'Charmeleon', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'
+  }
+
+   const item6 = {
+    id:6,
+    nome:'Charizard', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'
+  }
+
+  const item7 = {
+    id:7,
+    nome:'Squirtle', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+  }
+
+  const item8 = {
+    id:8,
+    nome:'Wartortle', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'
+  }
+
+  const item9 = {
+    id:9,
+    nome:'Blastoise', 
+    tipo: 'Planta',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'
+  }
+
+  const personagens = [item1, item2, item3, item4, item5, item6, item7, item8, item9]
   return (
     <>
        <div className='lista'>
--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,9 @@ function Card(props) {
     return(
         <>
             <div className='card'> 
-                <h2>{props.info.nome}</h2>
+                <h2>#: {props.info.id}</h2>
+                <h2>Nome: {props.info.nome}</h2>
+                <h2>Tipo: {props.info.tipo}</h2>
                 <img src={props.info.foto}/>
             </div>
         </>
